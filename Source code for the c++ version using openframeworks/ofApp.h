e09935d3db3f967c8f19a0a233ff3a6b4c41f064
Dhirodaatto <53444964+Dhirodaatto@users.noreply.github.com>
2020-12-08 21:05:14 +0530

    Add files via upload

---
+++ Source code for the c++ version using openframeworks/ofApp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		ofMesh mesh;
+
+		const int cell_length = 20;
+		const int height = 1000, width = 1000;
+		int nr = 50 , nc = 50;
+		double displacements[50][50];
+
+		vector<glm::vec3> vertices;
+		vector<int> indices;
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+};
